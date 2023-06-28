cb6d75a52039e211d6c9a2407b2189452ff89075
bellasoniadwi <bellasoniadwi@gmail.com>
2023-06-28 21:43:27 +0700

    Pertemuan 13 - 16 / Laravel API

---
+++ Pertemuan13-16/kelas3e-api/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+
+Route::post('products', [ProductsController::class, 'store'])->name('products.store');
+Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
